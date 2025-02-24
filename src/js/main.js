d42fc0ae0c2a8936042865630644de79db86154a
Alexkid <alexkid2001@rambler.ru>
2025-02-24 00:20:01 -0330

    added swiper.js. added fullscreen for gallery

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+
+const thumbnailsSwiper = new Swiper('.thumbnails-swiper', {
+    slidesPerView: 10,
+    spaceBetween: 10,
+    freeMode: true,
+    watchSlidesProgress: true,
+});
+
+const gallerySwiper = new Swiper('.gallery-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    thumbs: {
+        swiper: thumbnailsSwiper,
+    },
+});
+
+const fullscreenSwiper = new Swiper('.fullscreen-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+});
+
+document.querySelectorAll('.open-modal').forEach(image => {
+    image.addEventListener('click', () => {
+        document.getElementById('fullscreenModal').style.display = 'flex';
+    });
+});
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+    document.getElementById('fullscreenModal').style.display = 'none';
+});
